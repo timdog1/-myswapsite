6c65745d88d9f0c82a26264b19205120a978c49d
Oskar Karcz <oskar.karcz@rumblefish.pl>
2021-11-15 13:02:02 +0100

    refactor after cr

--- src/pages/Bridge/BridgeTransactionsSummary.tsx
+++ src/pages/Bridge/BridgeTransactionsSummary.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useRef } from 'react'
+import React, { useCallback } from 'react'
 import styled from 'styled-components'
 import { AdvancedDetailsFooter } from '../../components/AdvancedDetailsFooter'
 import { ButtonPrimary, ShowMoreButton } from '../../components/Button'
@@ -9,6 +9,83 @@ import { NETWORK_DETAIL } from '../../constants'
 import { useBridgeTxsFilter } from '../../state/bridge/hooks'
 import { BridgeTxsFilter } from '../../state/bridge/reducer'
 import { getExplorerLink } from '../../utils'
+
+const TableContainer = styled.div`
+  display: flex;
+  flex-flow: column;
+`
+
+const Header = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-flow: row;
+  padding: 10px;
+  font-size: 12px;
+  font-weight: 600;
+  line-height: 12px;
+  text-transform: uppercase;
+  color: ${props => props.theme.purple3};
+`
+
+const Body = styled.div`
+  flex-flow: column;
+  justify-content: space-between;
+`
+
+const Row = styled.div`
+  display: flex;
+  flex-flow: row;
+  justify-content: space-around;
+  padding: 0.25rem 0rem;
+  font-weight: 500;
+  font-size: 0.825rem;
+  color: ${({ theme }) => theme.text5};
+`
+
+const Column = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+const ColumnBridging = styled(Column)`
+  flex: 0 0 25%;
+`
+
+const ColumnFromTo = styled(Column)`
+  justify-content: center;
+`
+
+const ColumnFromToHeader = styled(Column)`
+  flex: 0 0 30%;
+  justify-content: space-between;
+`
+
+const Link = styled.a`
+  cursor: initial;
+  color: #0e9f6e;
+
+  &[href] {
+    cursor: pointer;
+  }
+
+  &[href]:hover {
+    text-decoration: underline;
+  }
+`
+
+const TextFrom = styled.div`
+  margin: 0px 7px 0px 0px;
+`
+
+const TextTo = styled(Link)<{ success: boolean }>`
+  color: ${({ success }) => (success ? '#0e9f6e' : '#8780bf')};
+  margin: 0px 0px 0px 7px;
+`
+
+const Dots = ({ success }: { success: boolean }) => {
+  return <div style={{ color: success ? '#0e9f6e' : '#8780bf' }}> &#183;&#183;&#183;&#183;</div>
+}
 
 interface BridgeTransactionsSummaryProps {
   transactions: BridgeTransactionSummary[]
@@ -60,91 +137,6 @@ export const BridgeTransactionsSummary = ({
   )
 }
 
-const TableContainer = styled.div`
-  display: flex;
-  flex-flow: column;
-`
-
-const Header = styled.div`
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  flex-flow: row;
-  padding: 10px;
-  font-size: 12px;
-  font-weight: 600;
-  line-height: 12px;
-  text-transform: uppercase;
-  color: ${props => props.theme.purple3};
-`
-
-const Body = styled.div`
-  flex-flow: column;
-  justify-content: space-between;
-`
-
-const Row = styled.div`
-  display: flex;
-  flex-flow: row;
-  justify-content: space-around;
-  padding: 0.25rem 0rem;
-  font-weight: 500;
-  font-size: 0.825rem;
-  color: ${({ theme }) => theme.text5};
-`
-
-const Column = styled.div`
-  display: flex
-  flex-flow: row;
-  align-items: center;
-`
-
-const ColumnBridging = styled(Column)`
-  flex-basis: 25%;
-`
-
-const ColumnFromTo = styled(Column)`
-  justify-content: center;
-`
-
-const ColumnFromToHeader = styled(Column)`
-  flex-basis: 30%;
-  justify-content: space-between;
-`
-
-const TextBridging = styled.div`
-  color=#ffffff;
-  font-size: 14px;
-  lineHeight: 14px; 
-  fontWeight: 600;
-`
-
-const Link = styled.a`
-  cursor: initial;
-  color: #0e9f6e;
-
-  &[href] {
-    cursor: pointer;
-  }
-
-  &[href]:hover {
-    text-decoration: underline;
-  }
-`
-
-const TextFrom = styled.div`
-  margin: 0px 7px 0px 0px;
-`
-
-const TextTo = styled(Link)<{ success: boolean }>`
-  color: ${({ success }) => (success ? '#0e9f6e' : '#8780bf')};
-  margin: 0px 0px 0px 7px;
-`
-
-const Dots = ({ success }: { success: boolean }) => {
-  return <div style={{ color: success ? '#0e9f6e' : '#8780bf' }}> &#183;&#183;&#183;&#183;</div>
-}
-
 interface BridgeTransactionsSummaryRow {
   tx: BridgeTransactionSummary
   onCollect: BridgeTransactionsSummaryProps['onCollect']
@@ -152,23 +144,19 @@ interface BridgeTransactionsSummaryRow {
 
 const BridgeTransactionsSummaryRow = ({ tx, onCollect }: BridgeTransactionsSummaryRow) => {
   const { assetName, fromChainId, status, toChainId, value, pendingReason, log } = tx
-
-  const refFrom = useRef<HTMLDivElement>(null)
-  const refTo = useRef<HTMLAnchorElement>(null)
-
   const success = status === 'confirmed' || status === 'claimed'
 
   return (
     <Row>
       <ColumnBridging>
-        <TextBridging>
+        <TYPE.main color="#ffffff" fontSize="14px" lineHeight="14px" display="inline">
           {value} {assetName}
-        </TextBridging>
+        </TYPE.main>
       </ColumnBridging>
       <ColumnFromTo>
         <Column>
           <TYPE.main color="text4" fontSize="10px" lineHeight="12px" display="inline">
-            <TextFrom ref={refFrom}>
+            <TextFrom>
               <Link
                 href={getExplorerLink(log[0].chainId, log[0].txHash, 'transaction')}
                 rel="noopener noreferrer"
@@ -180,14 +168,13 @@ const BridgeTransactionsSummaryRow = ({ tx, onCollect }: BridgeTransactionsSumma
           </TYPE.main>
         </Column>
         <Column>
-          <Dots success={true}></Dots>
-          <Dots success={success}></Dots>
+          <Dots success={true} />
+          <Dots success={success} />
         </Column>
         <Column>
           <TYPE.main color="text4" fontSize="10px" lineHeight="12px" display="inline">
             <TextTo
               success={success}
-              ref={refTo}
               href={log[1] && getExplorerLink(log[1].chainId, log[1].txHash, 'transaction')}
               rel="noopener noreferrer"
               target="_blank"
